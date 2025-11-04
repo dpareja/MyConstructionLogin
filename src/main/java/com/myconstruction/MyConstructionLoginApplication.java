213cf5530ecb99a44f1c8eb0cec7e9fc6b406a20
dpareja <daniel@dmtec.cl>
2025-11-03 23:37:54 -0300

    Initial commit: Sistema de login con Spring Boot

---
+++ src/main/java/com/myconstruction/MyConstructionLoginApplication.java
@@ -0,0 +1,11 @@
+package com.myconstruction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyConstructionLoginApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyConstructionLoginApplication.class, args);
+    }
+}
---
+++ src/test/java/com/myconstruction/MyConstructionLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myconstruction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyConstructionLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
